028f85bac3c47bb96154f32fcbb8488382d9a57f
inc0nc3ivabl3 <91997069+inc0nc3ivabl3@users.noreply.github.com>
2021-10-05 21:37:23 -0500

    Add files via upload

---
+++ coronavirus.py
@@ -0,0 +1,6 @@
+import  pyautogui, time
+for x in range (9999999999999999999999999):
+	pyautogui.write("""qQwWeErRtTyYuUiIoOp""")
+	pyautogui.write("""PaAsSdDfFgGhHjJkKlL;:'"zZxXcCvVbBnNmM,.? !1234567890""")
+	pyautogui.press('enter')
+	time.sleep(.01)

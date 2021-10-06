3db0bf4e3e566d892a66de71dc7f72818a2279a1
inc0nc3ivabl3 <91997069+inc0nc3ivabl3@users.noreply.github.com>
2021-10-05 21:37:13 -0500

    Delete coronavirus.py

--- coronavirus.py
+++
@@ -1,5 +0,0 @@
-import  pyautogui, time
-for x in range (200):
-	pyautogui.write("""qQwWeErRtTyYuUiIoOpPaAsSdDfFgGhHjJkKlL;:'"zZxXcCvVbBnNmM,.? !1234567890""")
-	pyautogui.press('enter')
-	time.sleep(.01)

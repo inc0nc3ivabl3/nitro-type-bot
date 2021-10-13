eb54b611fa82250a43cc590bc93b265b3e1f1319
inc0nc3ivabl3 <91997069+inc0nc3ivabl3@users.noreply.github.com>
2021-10-12 22:35:08 -0500

    Update coronavirus.py

--- coronavirus.py
+++ coronavirus.py
@@ -1,5 +1,15 @@
 import  pyautogui, time
-for x in range (9999999999999999999999999):
-	pyautogui.write("""qQwWeErRtTyYuUiIoOpPaAsSdDfFgGhHjJkKlL;:'"zZxXcCvVbBnNmM,.? !1234567890""")
-	pyautogui.press('enter')
-	time.sleep(.01)
+x = 0
+while True:
+	x += 1
+	click = x % 20 
+	pyautogui.press('enter') 
+	pyautogui.write("""qQwWeErRtTyYuUiIoOpPaAsSdDfFgGhHjJkKlL;:'"zZxXcCvVbBnNmM,.? !1234567890-""") #types the text
+	time.sleep(2)
+	if click == 0: 
+		pyautogui.click(x = 987, y = 955)	
+	if click != 0: 
+		{
+		}
+else:
+	print('why did u break the bot')

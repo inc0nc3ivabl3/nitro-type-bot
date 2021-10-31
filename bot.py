407c387c5bb38eea015ea813a316e5d14cfa2a8e
inc0nc3ivabl3 <91997069+inc0nc3ivabl3@users.noreply.github.com>
2021-10-30 21:09:50 -0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -3,13 +3,14 @@ from pyscreenshot import grab
 from time import sleep
 from os import environ
 from keyboard import read_key
-from sys import exit
 import pyautogui
 import pytesseract
 import random
-import os
 
-pytesseract.pytesseract.tesseract_cmd = r'C:\Users\USER\AppData\Local\Programs\Tesseract-OCR\tesseract.exe'
+print("What is the username of your pc?")
+user = input()
+print("Press enter to start the bot")
+pytesseract.pytesseract.tesseract_cmd = r'C:\Users' + '\\' + user + r'\AppData\Local\Programs\Tesseract-OCR\tesseract.exe'
 environ['OMP_THREAD_LIMIT'] = '1'
 
 #press enter to start
@@ -19,8 +20,13 @@ if read_key() == "enter":
 
 		#variables
 		h = random.uniform(.2, .25)
-		v = random.randint(1, 6)
+		#v = random.randint(1, 6)
 		z = 0
+
+		if h >= .225:
+			v = random.randint(4, 6)  
+		elif h < .225:
+			v = random.randint(1, 3)
 
 		#get screenshot
 		img = grab(bbox=(550, 723, 1350, 910))
@@ -51,5 +57,6 @@ if read_key() == "enter":
 		pyautogui.click(x=1243, y=861)
 		sleep(2)
 		pyautogui.click(x=805, y=615)
-		sleep(11)
+		sleep(12)
 		pyautogui.click(x = 992, y = 757)
+
